34db6e491edf1d0ab7eb21ef2286c35e1b1a642d
d-libards <dominiquelibardo09@gmail.com>
2024-12-11 02:05:46 +0800

    create form components done

--- components/form/ImageInput.tsx
+++ components/form/ImageInput.tsx
@@ -1,4 +1,15 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
 function ImageInput() {
-  return <div>ImageInput</div>;
+  const name = 'image';
+  return (
+    <div className='mb-2'>
+      <Label htmlFor={name} className='capitalize'>
+        Image
+      </Label>
+      <Input name={name} id={name} type='file' required accept='image/*' />
+    </div>
+  );
 }
 export default ImageInput;
--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -1,4 +1,26 @@
-function PriceInput() {
-  return <div>PriceInput</div>;
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+
+const name = 'price';
+type FormInputNumberProps = {
+  defaultValue?: number;
+};
+
+function PriceInput({ defaultValue }: FormInputNumberProps) {
+  return (
+    <div className='mb-2'>
+      <Label htmlFor='price' className='capitalize'>
+        Price ($)
+      </Label>
+      <Input
+        id={name}
+        type='number'
+        name={name}
+        min={0}
+        defaultValue={defaultValue || 100}
+        required
+      />
+    </div>
+  );
 }
 export default PriceInput;
